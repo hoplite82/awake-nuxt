d87ba47582e327c12806dc1d8546a7df48d695e9
hoplite82 <robert@pratersch.de>
2020-12-03 00:43:47 +0100

    showstartfilter _single

--- pages/categories/_single.vue
+++ pages/categories/_single.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="category-page" class="page-wrapper category-page">
-    <site-hero
+    <!-- <site-hero
       :title="$store.state.name"
       :subtitle="$store.state.description"
       :image="$store.state.image"
-    />
+    /> -->
     <main-section theme="sidebar-right">
       <template v-slot:default>
-        <!-- Posts in Category -->
-        <posts-grid :category="[$store.state.name]" :per-row="2" />
+        <h1 class="title">{{$store.state.name}}</h1>
+        <h2 class="subtitle">{{$store.state.description}}</h2>
+        <posts-grid :category="[$store.state.name]" :per-row="2" :number="50" />
       </template>
       <template v-slot:sidebar>
-        <h3 class="subtitle">
-          All Categories
-        </h3>
+        <h3 class="subtitle">All Categories</h3>
         <div class="panel">
           <nuxt-link
-            v-for="cat in allCats"
+            v-for="cat in catsfiltered"
             :key="cat.slug"
             :to="`/categories/${cat.slug}`"
             :class="{
               'panel-block': true,
-              'is-active': cat.slug === $route.params.single
+              'is-active': cat.slug === $route.params.single,
             }"
           >
             {{ cat.name }}
@@ -36,7 +35,7 @@ import { setPageData } from '../../helper'
 export default {
   data() {
     return {
-      allCats: []
+      allCats: [],
     }
   },
   fetch({ store, params }) {
@@ -44,6 +43,14 @@ export default {
   },
   async created() {
     this.allCats = await this.$cms.category.getAll()
-  }
+      .then((cats) => cats.filter((cat) => cat.name != 'showstart'))
+//      .then((cats) => cats.map((cat) => cat.name))
+  },
+  computed: {
+    catsfiltered() {
+      // console.log(this.allCats.filter((v) => v != 'showstart'))
+      return this.allCats
+    },
+  },
 }
 </script>
